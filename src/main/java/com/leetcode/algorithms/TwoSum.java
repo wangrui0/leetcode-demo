e368692f2cfd87dfc03a5d493b75ee439dd5b691
wangrui0 <1176306419@qq.com>
2019-12-16 20:10:36 +0800

    提交

--- src/main/java/com/leetcode/algorithms/TwoSum.java
+++ src/main/java/com/leetcode/algorithms/TwoSum.java
@@ -1,5 +1,7 @@
 package com.leetcode.algorithms;
 
+import java.util.HashMap;
+
 /**
  * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
  * <p>
@@ -18,34 +20,120 @@ package com.leetcode.algorithms;
  */
 public class TwoSum {
     public static void main(String[] args) {
+
         int[] nums = new int[]{2, 7, 11, 15};
         int target = 9;
-        int[] result = twoSum1(nums, target);
+//        int[] result = twoSum1(nums, target);
+        int[] result = twoSum3(nums, target);
 
-        print(result);
+        print(nums, result);
 
     }
 
+    /**
+     * 思想：
+     * 方法一：暴力法
+     * 暴力法很简单，遍历每个元素 x，并查找是否存在一个值与 target - x 相等的目标元素。
+     * 分析：
+     * 复杂度分析：
+     * <p>
+     * 时间复杂度：O(n^2)
+     * <p>
+     * 对于每个元素，我们试图通过遍历数组的其余部分来寻找它所对应的目标元素，这将耗费 O(n) 的时间。因此时间复杂度为 O(n^2)
+     * <p>
+     * 空间复杂度：O(1)。
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
     public static int[] twoSum1(int[] nums, int target) {
         if (nums == null || nums.length == 0) {
-            return null;
+            throw new IllegalArgumentException("参数错误!");
         }
         for (int i = 0; i <= nums.length - 1; i++) {
             for (int j = i + 1; j <= nums.length - 1; j++) {
                 if ((nums[i] + nums[j] == target)) {
-                    return new int[]{nums[i], nums[j]};
+                    return new int[]{i, j};
                 }
             }
         }
         return null;
     }
 
-    public static void print(int[] result) {
+    /**
+     * 方法二：两遍哈希表
+     * 为了对运行时间复杂度进行优化，我们需要一种更有效的方法来检查数组中是否存在目标元素。如果存在，我们需要找出它的索引。保持数组中的每个元素与其索引相互对应的最好方法是什么？哈希表。
+     * <p>
+     * 通过以空间换取速度的方式，我们可以将查找时间从 O(n) 降低到 O(1)。哈希表正是为此目的而构建的，它支持以 近似 恒定的时间进行快速查找。我用“近似”来描述，是因为一旦出现冲突，查找用时可能会退化到 O(n)。但只要你仔细地挑选哈希函数，在哈希表中进行查找的用时应当被摊销为 O(1)。
+     * <p>
+     * 一个简单的实现使用了两次迭代。在第一次迭代中，我们将每个元素的值和它的索引添加到表中。然后，在第二次迭代中，我们将检查每个元素所对应的目标元素（target - nums[i]）是否存在于表中。注意，该目标元素不能是 nums[i] 本身！
+     * <p>
+     * 分析：
+     * 复杂度分析：
+     * <p>
+     * 时间复杂度：O(n)
+     * 我们把包含有 n 个元素的列表遍历两次。由于哈希表将查找时间缩短到 O(1) ，所以时间复杂度为 O(n)。
+     * <p>
+     * 空间复杂度：O(n)，
+     * 所需的额外空间取决于哈希表中存储的元素数量，该表中存储了 n 个元素。
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] twoSum2(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int otherNum = target - nums[i];
+            if (map.containsKey(otherNum) && (map.get(otherNum) != i)) {
+                return new int[]{i, map.get(otherNum)};
+            }
+        }
+        throw new IllegalArgumentException("没有找到");
+    }
+
+    /**
+     * 方法三:
+     * <p>
+     * 事实证明，我们可以一次完成。在进行迭代并将元素插入到表中的同时，我们还会回过头来检查表中是否已经存在当前元素所对应的目标元素。如果它存在，那我们已经找到了对应解，并立即将其返回。
+     * <p>
+     * 分析：
+     * <p>
+     * 时间复杂度：O(n)，
+     * 我们只遍历了包含有 n 个元素的列表一次。在表中进行的每次查找只花费 O(1) 的时间。
+     * <p>
+     * 空间复杂度：O(n)，
+     * 所需的额外空间取决于哈希表中存储的元素数量，该表最多需要存储 nn 个元素。
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] twoSum3(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int otherNum = target - nums[i];
+            if (map.containsKey(otherNum) && (map.get(otherNum) != i)) {
+                return new int[]{i, map.get(otherNum)};
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        throw new IllegalArgumentException("没有找到");
+    }
+
+
+    public static void print(int[] nums, int[] result) {
         if (result == null || result.length == 0) {
-            return;
+            throw new IllegalArgumentException("参数错误!");
         }
         for (int i = 0; i <= result.length - 1; i++) {
-            System.out.println(result[i]);
+            System.out.println(nums[i]);
         }
     }
 }
